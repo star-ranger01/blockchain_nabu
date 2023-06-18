87a0475d282504a5a21ded4491103710ebcc318a
skyranger05 <ian.capri@live.com>
2023-06-17 20:33:35 -0700

    Main: edited CustomerAccount smart contract, added new setter function.

--- CustomerAccount.sol
+++ CustomerAccount.sol
@@ -9,6 +9,14 @@ contract CustomerAccount{
     string customerLastName;
 
     function getInfo() view public returns(address, bool, uint, string memory, string memory) {
-        return (owner, isNewAccount, accountBalance, customerName, customerLastName)
-    };
+        return (owner, isNewAccount, accountBalance, customerName, customerLastName);
+    }
+    
+    function setInfo(address owner, bool isNewAccount, uint accountBalance, string memory customerName, string memory customerLastName) public {
+        owner = newOwner;
+        isNewAccount = newAccountStatus;
+        accountBalance = newAccountBalance;
+        customerName = newCustomerName;
+        customerLastName = newCustomerLastName;
+    }
 }

d9a2fd9bc2c0a2366ff90195b3fe10389800f03d
star-ranger01 <star_ranger01@comcast.net>
2023-06-19 11:00:02 -0700

    Feature: edited the CustomerAccount smart contract.

--- CustomerAccount.sol
+++ CustomerAccount.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.5.0;
 
 // Create a customer contract to remit funds to others.
 contract CustomerAccount{
-    address owner;
+    address payable owner;
+    address payable authorizedRecepient;
     bool isNewAccount;
     uint public accountBalance;
     string customerName;
@@ -12,15 +13,17 @@ contract CustomerAccount{
         return (owner, isNewAccount, accountBalance, customerName, customerLastName);
     }
     
-    function setInfo(address newOwner, bool newAccountStatus, uint newAccountBalance, string memory newCustomerName, string memory newCustomerLastName) public {
+    function setInfo(address payable newOwner, address payable newAuthorizedRecepient, bool newAccountStatus, uint newAccountBalance, string memory newCustomerName, string memory newCustomerLastName) public {
         owner = newOwner;
-        isNewAccount = newAccountStatus;
+        authorizedRecepient = new authorizedRecepient;
+	isNewAccount = newAccountStatus;
         accountBalance = newAccountBalance;
         customerName = newCustomerName;
         customerLastName = newCustomerLastName;
     }
 
     function sendRemittance(uint amount, address payable recepient) public {
+       require(recepient == owner || recepient == authorizedRecipient , "The recepient address is not authorized!")
        recepient.transfer(amount);
        accountBalance = address(this).balance;
     }
